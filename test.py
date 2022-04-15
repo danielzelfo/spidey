5e1fb5de374203a033ad3d39d74870e96c5a472b
Daniel Zelfo <dzelfo@circinus-47.ics.uci.edu>
2022-04-15 03:18:18 -0700

    modified sleep time in worker thread based on time before last request

--- test.py
+++ test.py
@@ -1,41 +1,13 @@
-from lxml import html
-import requests
-from urllib.parse import urlparse, urljoin, urldefrag
 import timeit
-import numpy as np
+import time
 
-url = "https://en.wikipedia.org/wiki/Shor%27s_algorithm"
+timesl = 5
+
+start = timeit.default_timer()
+
+time.sleep(3)
 
 
-page = requests.get(url)
+newtime = timesl - timeit.default_timer() + start
 
-tree = html.fromstring(page.content)  
-  
-# Get element using XPath
-x = tree.xpath('.//a[@href]/@href')
-
-f = lambda x: urldefrag(urljoin(url, x))[0] #absolute_url(x)
-vf = np.vectorize(f)
-
-def test_map(x, n):
-    t = timeit.timeit(
-        'map(f, x)',
-        'from __main__ import x, f', number=n)
-    print('map: {0:.3f}'.format(t))
-
-def test_direct(x, n):
-    t = timeit.timeit(
-        'set([f(i) for i in x])',
-        'from __main__ import x, f', number=n)
-    print('direct: {0:.3f}'.format(t))
-
-def test_vectorized(x, n):
-    t = timeit.timeit(
-        'set(vf(x))',
-        'from __main__ import x, vf', number=n)
-    print('vectorized: {0:.3f}'.format(t))
-
-n = 1000
-# test_direct(x, n)      # 0.265
-# test_vectorized(x, n)  # 2.906
-test_map(x,n)
+print(newtime)
