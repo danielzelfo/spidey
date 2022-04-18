acde729dfee054367ad91f407556b861d65d194a
Jason Paul Wu <wujp1@circinus-33.ics.uci.edu>
2022-04-17 19:01:46 -0700

    sort query $ repeat

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import re
-from urllib.parse import urlparse, urljoin, urldefrag
+from urllib.parse import urlparse, urljoin, urldefrag,
 from lxml import html
 import re
 
@@ -17,7 +17,8 @@ bad_ext_path_pattern = re.compile(r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$")
 
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
+    links = sort_by_query(extract_next_links(url, resp))
+    
     return [link for link in links if is_valid(link)]
 
 def absolute_url(page_url, outlink_url):
@@ -35,17 +36,62 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    
+    print("extracting from: ", url)
     if resp.status != 200 or not resp.raw_response or not resp.raw_response.content or not is_valid(resp.url):
         return set()
     
     try:
-        tree = html.fromstring(resp.raw_response.content)
+        tree = html.fromstring(resp.raw_response.content) #check if urls are valid
     except:
         return set()
+
+    extracted = set([absolute_url(url, ol) for ol in tree.xpath('.//a[@href]/@href')])
+
+    print("extracted links: ", extracted)
     
-    return set([absolute_url(url, ol) for ol in tree.xpath('.//a[@href]/@href')])
+    return extracted
     
+# sort_by_query will sort querys
+# remove any urls with same query parameters
+# pointless to run if majority of links have no query parameters
+
+def sort_by_query(links):
+    map = {} # Store sorted links
+    sorted_links = [] # Return 
+
+    for url in links:
+        parsed = urlparse(url)
+        # extract/sort query 
+        query = parsed.query.split("&")
+        query.sort()
+        query_string = ""
+        # build new query string
+        for q in query:
+            query_string += q
+        print("query_string", query_string)
+        # url with sorted query
+        new_url = parsed.scheme + parsed.netloc + parsed.path + query_string
+
+        #check if url exists already, if not add to return list
+        if new_url not in map:
+            map[new_url] = 1
+            sorted_links.append(new_url)
+    
+    return sorted_links
+
+
+
+#Pass in a url path
+#Detects repeating path trap
+# def contains_repeating_path(url_path):
+#     map = {}
+#     spilt_path = url_path.split("/")[1:]
+#     for path in spilt_path:
+#         if path in map:
+#             return False
+#         else:
+#             map[path] = 1
+#     return True
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -53,7 +99,9 @@ def is_valid(url):
     # There are already some conditions that return False.
     try:
         parsed = urlparse(url)
-        return scheme_pattern.match(parsed.scheme.lower()) and netloc_pattern.match(parsed.netloc.lower()) and not bad_ext_path_pattern.match(parsed.path.lower())
+        return (scheme_pattern.match(parsed.scheme.lower()) 
+                and netloc_pattern.match(parsed.netloc.lower()) 
+                and not bad_ext_path_pattern.match(parsed.path.lower()))
     except TypeError:
         print ("TypeError for ", parsed)
         raise
