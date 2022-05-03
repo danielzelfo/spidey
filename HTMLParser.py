81f88a2efba4ef5dbc5471b14d4cd89339bfcb39
Daniel Zelfo <dzelfo@uci.edu>
2022-05-03 16:03:35 -0700

    added code

---
+++ HTMLParser.py
@@ -0,0 +1,9 @@
+class HTMLParser:
+    def __init__(self):
+        pass
+
+    def tonenize(self, textcontent):
+        pass
+
+    def extract_text(self, contnet):
+        pass
---
+++ Indexer.py
@@ -0,0 +1,19 @@
+class Indexer:
+    def __init__(self):
+        self.alpha_position_index = {}
+        pass
+    
+    def index(self, filepath):
+        # parse
+        # stem
+        # ...
+        # write to disk
+        pass
+    
+    def write_to_disk(self, stem, document, documentposition, terms, frequency):
+        # open the disk file
+        # check if stem was indexed before
+        # if it is
+        #     append to end of line ", {document, documentposition, terms, frequency}"
+        # if not, add to end of file
+        # format: "stem: {document, documentposition, terms, frequency}"
