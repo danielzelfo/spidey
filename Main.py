5224daf67e9c62342889339b0ce04c086fe09a20
Jason Paul Wu <wujp1@circinus-38.ics.uci.edu>
2022-05-05 15:59:44 -0700

    doc freq, doc pos

--- Main.py
+++ Main.py
@@ -6,14 +6,19 @@ import warnings
 warnings.filterwarnings("ignore")
 
 directory = "page_data/data/DEV/"
+
+#testDirectory = "testing/"
+
 indexer = None
 
 def run():
+    
     run_log = open("run_log.txt", "a")
     run_log.write("RUN STARTED AT: " + str(datetime.datetime.now()) + "\n")
 
     indexer = Indexer(run_log=run_log)
     files = list(Path(directory).rglob('*.json'))
+
     with alive_bar(len(files), force_tty=True) as bar:
         for filepath in files:
             # print("current file: ", filepath)
@@ -23,7 +28,7 @@ def run():
     
     print("Merging database files...")
     indexer.k_way_merge_files()
-    
+    indexer.save_urls()
     run_log.write("RUN ENDED AT: " + str(datetime.datetime.now()) + "\n")
     run_log.close()
 
