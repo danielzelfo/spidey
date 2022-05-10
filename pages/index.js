acead5fe159b75053ee590441f099f0384d0e86d
Daniel Zelfo <dzelfo@uci.edu>
2022-05-10 02:12:10 -0700

    Added initial HTML / CSS

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useState, useMemo } from 'react';
 
 export default function Home() {
+  const [isFocused, setIsFocused] = useState(false);
+  const headerClassName = isFocused
+        ? "header wide"
+        : "header";
+  
+  const eventHandlers = useMemo(() => ({
+    onFocus: () => setIsFocused(true),
+    onBlur: () => setIsFocused(false),
+  }), []);
+    
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Spidey Search</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="/style.css" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <div id="app">
+          <div className={headerClassName}>
+            <div className="titleContainer">
+              <h1 className="title">Spidey Search</h1>
+            </div>
+            <div className="searchBarContainer">
+              <input className='searchBar' {...eventHandlers} placeholder="Search" />
+            </div>
+          </div>
+          <div className="results">
+          </div>
+        </div>
       </main>
-
-      <Footer />
     </div>
   )
 }
